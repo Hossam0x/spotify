3b2f2fb6dc155d6230127d22316f227c44bb49c6
hossam dif <hossamrashed17@gmail.com>
2024-12-12 22:23:15 +0200

    splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/app_theme.dart';
+import 'package:spotify/presentation/pages/splash/splash.dart';
 
 void main() {
   runApp(const Spotify());
@@ -9,6 +11,10 @@ class Spotify extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
+    );
   }
 }
