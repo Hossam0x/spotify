91e00d0eb5b6fe09735e684c268ba4e069b68d3f
hossam dif <hossamrashed17@gmail.com>
2024-12-14 02:21:04 +0200

    adding info of user to firestore collection

--- lib/data/sources/auth/auth__firebase_service.dart
+++ lib/data/sources/auth/auth__firebase_service.dart
@@ -1,28 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:spotify/data/models/auth/create_user_req.dart';
 import 'package:spotify/data/models/auth/signIn_user_req.dart';
 
-abstract class AuthFirebaseService{
-    Future<Either>signup(CreateUserReq createUserReq);
-      Future<Either>signIn(SigninUserReq signInUserReq);
-
-
+abstract class AuthFirebaseService {
+  Future<Either> signup(CreateUserReq createUserReq);
+  Future<Either> signIn(SigninUserReq signInUserReq);
 }
 
-class FirebaseAuthServiceImpl extends AuthFirebaseService{
+class FirebaseAuthServiceImpl extends AuthFirebaseService {
   @override
-  Future<Either> signIn(SigninUserReq signInUserReq) async{
-    try{
+  Future<Either> signIn(SigninUserReq signInUserReq) async {
+    try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: signInUserReq.email,
-       password: signInUserReq.password
-       );
-       return const Right("Welcome Again..");
-    }
-    on FirebaseAuthException catch(e)
-    {
+          email: signInUserReq.email, password: signInUserReq.password);
+      return const Right("Welcome Again..");
+    } on FirebaseAuthException catch (e) {
       String message = "";
       if (e.code == 'invalid-credentials') {
         message = 'Wrong Password providing for that user';
@@ -38,26 +33,24 @@ class FirebaseAuthServiceImpl extends AuthFirebaseService{
   }
 
   @override
-  Future<Either> signup(CreateUserReq createUserReq)async {
-    try{
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: createUserReq.email,
-         password: createUserReq.password
-         );
-         return const Right("Signup successful");
-
-    }on FirebaseAuthException catch (e) {
-      String message ="" ;
-      if(e.code == 'weak-password'){
+  Future<Either> signup(CreateUserReq createUserReq) async {
+    try {
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: createUserReq.email, password: createUserReq.password);
+      await FirebaseFirestore.instance.collection('Users').add({
+        'email': data.user?.email,
+        'fullname': createUserReq.fullName,
+        'uid': data.user?.uid,
+      });
+      return const Right("Signup successful");
+    } on FirebaseAuthException catch (e) {
+      String message = "";
+      if (e.code == 'weak-password') {
         message = "Password Provided is too weak";
-      }
-      else if (e.code == 'email-aleady-in-use'){
+      } else if (e.code == 'email-aleady-in-use') {
         message = 'An account already exists with that email';
       }
       return Left(message);
-
     }
   }
-
-
 }
